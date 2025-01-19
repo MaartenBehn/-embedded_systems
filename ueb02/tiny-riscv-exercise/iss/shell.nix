c2df86a6d17b1bca6561f628dee5262643bad9b7
Maarten Behn <maarten.behn@gmail.com>
2025-01-19 16:57:35 +0100

    M instuction

--- ueb02/tiny-riscv-exercise/iss/shell.nix
+++ ueb02/tiny-riscv-exercise/iss/shell.nix
@@ -1,18 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  riscv32-pkgs = import <nixpkgs> {
-    crossSystem = (import <nixpkgs/lib>).systems.examples.riscv32-embedded;
-  };
-
-  riscv64-pkgs = import <nixpkgs> {
-    crossSystem = (import <nixpkgs/lib>).systems.examples.riscv64;
-  };
-
-in
 pkgs.mkShell rec {
 
-  name = "riscv32";
+  name = "iss";
 
   packages = with pkgs; [
     digital
--- ueb02/tiny-riscv-exercise/sw/run_tests.sh
+++ ueb02/tiny-riscv-exercise/sw/run_tests.sh
@@ -1,14 +1,29 @@
+echo " --- asm-example --- "
 cd asm-example
 make clean 
 make sim
 cd ..
 
+echo " --- c-example --- "
 cd c-example
 make clean 
 make sim 
 cd ..
 
+echo " --- multiplication-add --- "
 cd multiplication-add
+make clean 
+make sim 
+cd ..
+
+echo " --- multiplication-mul --- "
+cd multiplication-mul
+make clean 
+make sim 
+cd ..
+
+echo " --- factorize-custom --- "
+cd factorize-custom
 make clean 
 make sim 
 cd ..
